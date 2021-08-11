a0a7366694f9036446c97f88e2d40ba5325ed851
Colugo <68328892+colugomusic@users.noreply.github.com>
2021-08-11 23:55:28 +0100

    Update macros.hpp

--- include/gdnutil/macros.hpp
+++ include/gdnutil/macros.hpp
@@ -1,3 +1,9 @@
 #pragma once
 
 #define GDN_REG_METHOD(Type, Name) register_method(#Name, &Type::Name)
+#define GDN_REG_SLOT(Type, Name) register_method("_"#Name, &Type::_##Name)
+#define GDN_REG_PROPERTY(Class, Type, Name, Default) register_property<Class, Type>(#Name, &Class::##Name##_, Default)
+#define GDN_REG_PROPERTY_SETGET(Class, Type, Name, Default) register_property<Class, Type>(#Name, &Class::set_##Name, &Class::get_##Name, Default)
+#define GDN_SIGNAL(Name) static constexpr auto Name = #Name
+#define GDN_SLOT(Name, Args) void _##Name Args; static constexpr auto Name = "_"#Name
+#define GDN_PROPERTY(Type, Name) Type Name##_
