9934d5e6e44ffbb76dc2cb403f1f7273c1a19245
Colugo <68328892+colugomusic@users.noreply.github.com>
2021-09-01 14:58:05 +0100

    Update macros.hpp

--- include/gdnutil/macros.hpp
+++ include/gdnutil/macros.hpp
@@ -6,4 +6,5 @@
 #define GDN_REG_PROPERTY_SETGET(Class, Type, Name, Default) register_property<Class, Type>(#Name, &Class::set_##Name, &Class::get_##Name, Default)
 #define GDN_SIGNAL(Name) static constexpr auto Name = #Name
 #define GDN_SLOT(Name, Args) static constexpr auto Name = "_"#Name; void _##Name Args
+#define GDN_VIRTUAL_SLOT(Name, Args) static constexpr auto Name = "_"#Name; virtual void _##Name Args
 #define GDN_PROPERTY(Type, Name) Type Name##_
