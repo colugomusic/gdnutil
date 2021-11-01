5d8780e4f6b0bb64ec6046222e8155f022e86411
Colugo <68328892+colugomusic@users.noreply.github.com>
2021-11-01 16:03:02 +0000

    Add GDN_BUILTIN_SLOT

--- include/gdnutil/macros.hpp
+++ include/gdnutil/macros.hpp
@@ -5,6 +5,7 @@
 #define GDN_REG_PROPERTY(Class, Type, Name, Default) register_property<Class, Type>(#Name, &Class::##Name##_, Default)
 #define GDN_REG_PROPERTY_SETGET(Class, Type, Name, Default) register_property<Class, Type>(#Name, &Class::set_##Name, &Class::get_##Name, Default)
 #define GDN_SIGNAL(Name) static constexpr auto Name = #Name
+#define GDN_BUILTIN_SLOT(Name) static constexpr auto Name##_ = #Name
 #define GDN_SLOT(Name, Args) static constexpr auto Name = "_"#Name; void _##Name Args
 #define GDN_VIRTUAL_SLOT(Name, Args) static constexpr auto Name = "_"#Name; virtual void _##Name Args
 #define GDN_PROPERTY(Type, Name) Type Name##_
