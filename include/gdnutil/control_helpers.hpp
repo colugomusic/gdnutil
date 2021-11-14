004a4f24cc6608c40eae838f24cc7c223ab14836
Colugo <68328892+colugomusic@users.noreply.github.com>
2021-11-14 15:13:00 +0000

    Update control_helpers.hpp

--- include/gdnutil/control_helpers.hpp
+++ include/gdnutil/control_helpers.hpp
@@ -11,88 +11,112 @@ inline auto width(const godot::Control* c)
 	return c->get_size().width;
 }
 
-inline auto height(godot::Control* c)
+inline auto height(const godot::Control* c)
 {
 	return c->get_size().height;
 }
 
-inline auto left(godot::Control* c)
+inline auto left(const godot::Control* c)
 {
 	return c->get_position().x;
 }
 
-inline auto right(godot::Control* c)
+inline auto right(const godot::Control* c)
 {
 	return c->get_position().x + c->get_size().width;
 }
 
-inline auto center(godot::Control* c)
+inline auto center(const godot::Control* c)
 {
 	return c->get_position() + (c->get_size() * 0.5f);
 }
 
-inline auto top_middle(godot::Control* c)
+inline auto top_left(const godot::Control* c)
+{
+	return c->get_position();
+}
+
+inline auto top_middle(const godot::Control* c)
 {
 	return c->get_position() + godot::Vector2(width(c) * 0.5f, 0.0f);
 }
 
-inline auto top(godot::Control* c)
+inline auto bottom_left(const godot::Control* c)
+{
+	return c->get_position() + godot::Vector2 { 0.0f, c->get_size().height };
+}
+
+inline auto top(const godot::Control* c)
 {
 	return c->get_position().y;
 }
 
-inline auto bottom(godot::Control* c)
+inline auto bottom(const godot::Control* c)
 {
 	return c->get_position().y + c->get_size().height;
 }
 
-inline auto end(godot::Control* c)
+inline auto end(const godot::Control* c)
 {
 	return c->get_position() + c->get_size();
 }
 
 namespace global {
 
-inline auto left(godot::Control* c)
+inline auto left(const godot::Control* c)
 {
 	return c->get_global_position().x;
 }
 
-inline auto right(godot::Control* c)
+inline auto right(const godot::Control* c)
 {
 	return c->get_global_position().x + c->get_size().width;
 }
 
-inline auto center(godot::Control* c)
+inline auto center(const godot::Control* c)
 {
 	return c->get_global_position() + (c->get_size() * 0.5f);
 }
 
-inline auto top_middle(godot::Control* c)
+inline auto top_middle(const godot::Control* c)
 {
 	return c->get_global_position() + godot::Vector2(width(c) * 0.5f, 0.0f);
 }
 
-inline auto top(godot::Control* c)
+inline auto top(const godot::Control* c)
 {
 	return c->get_global_position().y;
 }
 
-inline auto bottom(godot::Control* c)
+inline auto bottom(const godot::Control* c)
 {
 	return c->get_global_position().y + c->get_size().height;
 }
 
-inline auto bottom_left(godot::Control* c)
+inline auto bottom_left(const godot::Control* c)
 {
 	return c->get_global_position() + godot::Vector2{0.0f, c->get_size().height};
 }
 
-inline auto end(godot::Control* c)
+inline auto end(const godot::Control* c)
 {
 	return c->get_global_position() + c->get_size();
 }
 
 } // global
+
+inline void set_opacity(godot::Control* c, float opacity)
+{
+	auto modulate = c->get_modulate();
+
+	modulate.a = opacity;
+
+	c->set_modulate(modulate);
+}
+
+inline void bring_to_front(godot::Control* c)
+{
+	c->get_parent()->move_child(c, c->get_parent()->get_child_count());
+}
 
 } // color_util
