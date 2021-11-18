e38e13c425d6d655a85a6fe12ff83699d09ee806
Colugo <68328892+colugomusic@users.noreply.github.com>
2021-11-18 18:25:22 +0000

    Update macros.hpp

--- include/gdnutil/macros.hpp
+++ include/gdnutil/macros.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
-#define GDN_REG_METHOD(Type, Name) register_method(#Name, &Type::Name)
-#define GDN_REG_SLOT(Type, Name) register_method("_"#Name, &Type::_##Name)
-#define GDN_REG_PROPERTY(Class, Type, Name, Default) register_property<Class, Type>(#Name, &Class::##Name##_, Default)
-#define GDN_REG_PROPERTY_SETGET(Class, Type, Name, Default) register_property<Class, Type>(#Name, &Class::set_##Name, &Class::get_##Name, Default)
+#pragma warning(push, 0)
+#include <Godot.hpp>
+#pragma warning(pop)
+
+#define GDN_REG_METHOD(Name) register_method(#Name, &GDN_THIS_CLASS::Name)
+#define GDN_REG_SLOT(Name) register_method("_"#Name, &GDN_THIS_CLASS::_##Name)
+#define GDN_REG_PROPERTY(Type, Name, Default) register_property<GDN_THIS_CLASS, Type>(#Name, &GDN_THIS_CLASS::##Name##_, Default)
+#define GDN_REG_PROPERTY_SETGET(Type, Name, Default) register_property<GDN_THIS_CLASS, Type>(#Name, &GDN_THIS_CLASS::set_##Name, &GDN_THIS_CLASS::get_##Name, Default)
 #define GDN_SIGNAL(Name) static constexpr auto Name = #Name
 #define GDN_BUILTIN_SLOT(Name) static constexpr auto Name##_ = #Name
 #define GDN_SLOT(Name, Args) static constexpr auto Name = "_"#Name; void _##Name Args
 #define GDN_VIRTUAL_SLOT(Name, Args) static constexpr auto Name = "_"#Name; virtual void _##Name Args
 #define GDN_PROPERTY(Type, Name) Type Name##_
 #define GDN_GET_NODE(Parent, Type, Name) Name = gdn::tree::get<Type>(Parent, #Name)
+#define GDN_CLASS(Name, Base) GODOT_CLASS(Name, Base); using GDN_THIS_CLASS = Name
