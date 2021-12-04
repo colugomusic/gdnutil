c0cca4fb2c5c0508a63f261c4699e660d97e622e
Colugo <68328892+colugomusic@users.noreply.github.com>
2021-12-04 02:03:28 +0000

    Add GDN_REG_ENUM_PROPERTY_SETGET

--- include/gdnutil/macros.hpp
+++ include/gdnutil/macros.hpp
@@ -8,6 +8,7 @@
 #define GDN_REG_SLOT(Name) register_method("_"#Name, &GDN_THIS_CLASS::_##Name)
 #define GDN_REG_PROPERTY(Type, Name, Default) register_property<GDN_THIS_CLASS, Type>(#Name, &GDN_THIS_CLASS::##Name##_, Default)
 #define GDN_REG_PROPERTY_SETGET(Type, Name, Default) register_property<GDN_THIS_CLASS, Type>(#Name, &GDN_THIS_CLASS::set_##Name, &GDN_THIS_CLASS::get_##Name, Default)
+#define GDN_REG_ENUM_PROPERTY_SETGET(Name, Default, Enumerators) register_property<GDN_THIS_CLASS, int>(#Name, &GDN_THIS_CLASS::set_##Name, &GDN_THIS_CLASS::get_##Name, int(Default), GODOT_METHOD_RPC_MODE_DISABLED, GODOT_PROPERTY_USAGE_DEFAULT, GODOT_PROPERTY_HINT_ENUM, Enumerators)
 #define GDN_SIGNAL(Name) static constexpr auto Name = #Name
 #define GDN_BUILTIN_SLOT(Name) static constexpr auto Name##_ = #Name
 #define GDN_SLOT(Name, Args) static constexpr auto Name = "_"#Name; void _##Name Args
