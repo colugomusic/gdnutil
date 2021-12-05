c91a0b1ed27e925e964249dc5e060e6b36e95518
Colugo <68328892+colugomusic@users.noreply.github.com>
2021-12-05 02:27:39 +0000

    Create hover_status.hpp

---
+++ include/gdnutil/hover_status.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <functional>
+#include <Control.hpp>
+#include <Input.hpp>
+
+namespace gdn {
+
+class HoverStatus
+{
+public:
+
+	using OnChanged = std::function<void()>;
+
+	void operator()(godot::Control* control, int what)
+	{
+		switch (what)
+		{
+			case godot::Control::NOTIFICATION_MOUSE_ENTER:
+			{
+				set(true);
+				return;
+			}
+
+			case godot::Control::NOTIFICATION_MOUSE_EXIT:
+			case godot::Control::NOTIFICATION_EXIT_TREE:
+			case godot::Control::NOTIFICATION_WM_FOCUS_OUT:
+			case godot::Control::NOTIFICATION_WM_MOUSE_EXIT:
+			{
+				set(false);
+				return;
+			}
+
+			case godot::Control::NOTIFICATION_POST_ENTER_TREE:
+			case godot::Control::NOTIFICATION_VISIBILITY_CHANGED:
+			{
+				set(get_hover_status_from_rect_if_visible(control));
+				return;
+			}
+		}
+	}
+
+	operator bool() const { return status_; }
+
+	OnChanged on_changed;
+
+private:
+
+	void set(bool yes)
+	{
+		if (init_ && status_ == yes) return;
+
+		status_ = yes;
+
+		if (on_changed)
+		{
+			init_ = true;
+			on_changed();
+		}
+	}
+
+	static bool get_hover_status_from_rect(godot::Control* control)
+	{
+		return control->get_global_rect().has_point(control->get_global_mouse_position());
+	}
+
+	static bool get_hover_status_from_rect_if_visible(godot::Control* control)
+	{
+		if (!control->is_visible_in_tree()) return false;
+
+		return get_hover_status_from_rect(control);
+	}
+
+	bool init_ { false };
+	bool status_ { false };
+};
+
+} // gdn
