f21ea864825dac406e13780955491a63b64cf2ba
Colugo <68328892+colugomusic@users.noreply.github.com>
2022-04-02 16:26:55 +0100

    Create hacks.hpp

---
+++ include/gdnutil/hacks.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <String.hpp>
+#include <GodotGlobal.hpp>
+
+namespace gdn {
+namespace hacks {
+
+// Workaround for https://github.com/godotengine/godot/issues/40957
+inline std::string to_utf8(godot::String s)
+{
+	const auto c_string = s.alloc_c_string();
+
+	std::string out(c_string);
+
+	godot::api->godot_free(c_string);
+
+	return out;
+}
+
+} // hacks
+} // gdn
