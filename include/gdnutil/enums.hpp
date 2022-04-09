c7df01effdb9872d2b71a18535d0330b2a8694e9
Colugo <68328892+colugomusic@users.noreply.github.com>
2022-04-09 20:14:23 +0100

    Create enums.hpp

---
+++ include/gdnutil/enums.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace gdn {
+namespace enums {
+
+namespace TextureRect {
+
+static constexpr auto StretchMode = ""
+	"Scale On Expand,"
+	"Scale,"
+	"Tile,"
+	"Keep,"
+	"Keep Centered,"
+	"Keep Aspect,"
+	"Keep Aspect Centered,"
+	"Keep Aspect Covered"
+;
+
+} // TextureRect
+
+} // enums
+} // gdn
