1264dd75aca66b84bdd8b135ec37f4013c7f15dd
Colugo <68328892+colugomusic@users.noreply.github.com>
2022-04-17 10:56:46 +0100

    Create packed_scene.hpp

---
+++ include/gdnutil/packed_scene.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Object.hpp>
+#include <PackedScene.hpp>
+#include <ResourceLoader.hpp>
+
+namespace gdn {
+
+template <class T>
+class PackedScene
+{
+public:
+
+	PackedScene(godot::String path)
+		: scene_{ godot::ResourceLoader::get_singleton()->load(path) }
+	{
+	}
+
+	auto instance() const
+	{
+		return Object::cast_to<T>(scene_->instance());
+	}
+
+private:
+
+	godot::Ref<godot::PackedScene> scene_;
+};
+
+} // gdn
