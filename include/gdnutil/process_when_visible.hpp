f0c8f58f6368998b2080d8dee109d15806a139c7
Colugo <68328892+colugomusic@users.noreply.github.com>
2022-09-08 18:03:04 +0100

    Create process_when_visible.hpp

---
+++ include/gdnutil/process_when_visible.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <Array.hpp>
+#include <Control.hpp>
+#include "macros.hpp"
+
+namespace gdn {
+
+class ProcessWhenVisible : public godot::Reference
+{
+	GDN_CLASS(ProcessWhenVisible, godot::Reference);
+
+public:
+
+	static auto _register_methods() -> void
+	{
+		GDN_REG_SLOT(on_visibility_changed);
+	}
+
+	auto add(godot::Control* control) -> void
+	{
+		control->connect(visibility_changed, this, on_visibility_changed, godot::Array::make(control));
+	}
+
+	auto _init() -> void {}
+
+private:
+
+	GDN_SIGNAL(visibility_changed);
+
+	GDN_SLOT(on_visibility_changed, (godot::Control* control))
+	{
+		control->set_process(control->is_visible_in_tree());
+	}
+};
+} // gdn
