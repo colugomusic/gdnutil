4fad4769fa6bc95d1bd8702a0ee4cc2b777d979b
Colugo <68328892+colugomusic@users.noreply.github.com>
2022-10-21 03:28:48 +0100

    Update memory.hpp

--- include/gdnutil/memory.hpp
+++ include/gdnutil/memory.hpp
@@ -13,5 +13,23 @@ auto make_shared()
 	return std::shared_ptr<T>(T::_new(), [](T* o) { o->free(); });
 }
 
+template <typename T>
+struct deleter
+{
+	auto operator()(T* object) -> void
+	{
+		object->free();
+	}
+};
+
+template <typename T>
+using unique = std::unique_ptr<T, deleter<T>>;
+
+template <typename T>
+auto make_unique() -> unique<T>
+{
+	return unique<T>(T::_new());
+}
+
 } // memory
 } // gdn
