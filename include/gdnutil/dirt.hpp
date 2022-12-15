0219f7890d325ae40c4a9f8b9bf201659b060a42
Colugo <68328892+colugomusic@users.noreply.github.com>
2022-12-15 18:34:58 +0000

    Create dirt.hpp

---
+++ include/gdnutil/dirt.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <CanvasItem.hpp>
+
+namespace gdn {
+
+class Dirt
+{
+public:
+
+	Dirt() = default;
+
+	Dirt(godot::CanvasItem* parent)
+		: parent_{parent}
+	{
+	}
+
+	auto set_dirty() -> void
+	{
+		dirty_ = true;
+
+		if (parent_)
+		{
+			parent_->update();
+		}
+	}
+
+	auto check_and_reset() -> bool
+	{
+		const auto out{dirty_};
+
+		dirty_ = false;
+
+		return out;
+	}
+
+private:
+
+	bool dirty_{true};
+	godot::CanvasItem* parent_{};
+};
+
+} // gdn
