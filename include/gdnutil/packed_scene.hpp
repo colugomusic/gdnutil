1eb557b63959dc0dfce8556aad2f0ed717992c9c
Colugo <68328892+colugomusic@users.noreply.github.com>
2023-04-29 21:53:22 +0100

    Update packed_scene.hpp

--- include/gdnutil/packed_scene.hpp
+++ include/gdnutil/packed_scene.hpp
@@ -10,34 +10,15 @@ template <class T>
 class PackedScene
 {
 public:
-
 	PackedScene() = default;
 	PackedScene(const PackedScene& rhs) = default;
-	PackedScene(PackedScene&& rhs) noexcept
-		: scene_{rhs.scene_}
-	{
-	}
-
+	PackedScene(PackedScene&& rhs) noexcept : scene_{rhs.scene_} {}
 	auto operator=(const PackedScene& rhs) -> PackedScene& = default;
-	auto operator=(PackedScene&& rhs) noexcept -> PackedScene&
-	{
-		scene_ = rhs.scene_;
-
-		return *this;
-	}
-
-	PackedScene(godot::String path)
-		: scene_{ godot::ResourceLoader::get_singleton()->load(path) }
-	{
-	}
-
-	auto instance() const
-	{
-		return godot::Object::cast_to<T>(scene_->instance());
-	}
-
+	auto operator=(PackedScene&& rhs) noexcept -> PackedScene& { scene_ = rhs.scene_; return *this; }
+	PackedScene(godot::String path) : scene_{ godot::ResourceLoader::get_singleton()->load(path) } {}
+	auto instance() const { return godot::Object::cast_to<T>(scene_->instance()); }
+	operator bool() const { return scene_.is_valid(); }
 private:
-
 	godot::Ref<godot::PackedScene> scene_;
 };
 
