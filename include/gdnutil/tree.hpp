54e0eaee1441eb5130ec3685ccb7cebe4eb63386
Colugo <68328892+colugomusic@users.noreply.github.com>
2023-06-05 00:18:52 +0100

    Update tree.hpp

--- include/gdnutil/tree.hpp
+++ include/gdnutil/tree.hpp
@@ -12,87 +12,76 @@
 namespace gdn {
 namespace tree {
 
-inline godot::Node* get(godot::Viewport* root, godot::NodePath path)
-{
-	if (!root->has_node(path)) return nullptr;
-
+inline godot::Node* get(godot::Viewport* root, godot::NodePath path) {
+	if (!root->has_node(path)) {
+		return nullptr;
+	}
 	return root->get_node(path);
 }
 
-inline godot::Node* get(godot::SceneTree* main_tree, godot::NodePath path)
-{
+inline godot::Node* get(godot::SceneTree* main_tree, godot::NodePath path) {
 	return get(main_tree->get_root(), path);
 }
 
-inline godot::SceneTree* scene_tree()
-{
+inline godot::SceneTree* scene_tree() {
 	return godot::Object::cast_to<godot::SceneTree>(godot::Engine::get_singleton()->get_main_loop());
 }
 
-inline godot::Node* get(godot::NodePath path)
-{
+inline godot::Node* get(godot::NodePath path) {
 	return get(scene_tree(), path);
 }
 
-inline godot::Node* get(godot::Node* parent, godot::NodePath path)
-{
+inline godot::Node* get(godot::Node* parent, godot::NodePath path) {
 	return parent->get_node(path);
 }
 
 template <class T>
-T* get(godot::Node* parent, godot::NodePath path)
-{
+T* get(godot::Node* parent, godot::NodePath path) {
 	return godot::Object::cast_to<T>(get(parent, path));
 }
 
 template <class T>
-T* get(godot::NodePath path)
-{
+T* get(godot::NodePath path) {
 	return godot::Object::cast_to<T>(get(path));
 }
 
 template <class T>
-auto get(godot::Node* parent, godot::NodePath path, T** out) -> void
-{
+auto get(godot::Node* parent, godot::NodePath path, T** out) -> void {
 	*out = get<T>(parent, path);
 }
 
 template <class T>
-T* require(godot::Node* parent, godot::NodePath path)
-{
+T* require(godot::Node* parent, godot::NodePath path) {
 	const auto out { get<T>(parent, path) };
-
 	assert (out);
-
 	return out;
 }
 
 template <class T>
-T* require(godot::NodePath path)
-{
+T* require(godot::NodePath path) {
 	const auto out { get<T>(path) };
-
 	assert (out);
-
 	return out;
 }
 
 template <class T>
-auto require(godot::Node* parent, godot::NodePath path, T** out) -> void
-{
+auto require(godot::Node* parent, godot::NodePath path, T** out) -> void {
 	*out = require<T>(parent, path);
 }
 
+template <class T>
+auto require(godot::NodePath path, T** out) -> void {
+	*out = require<T>(path);
+}
+
 template <typename T, typename... Args>
-auto require(godot::Node* parent, godot::NodePath path, T* out, Args&&... args) -> void
-{
+auto require(godot::Node* parent, godot::NodePath path, T* out, Args&&... args) -> void {
 	auto node{require<typename T::control_type>(parent, path)};
 	*out = T(node, std::forward<Args>(args)...);
 }
 
 template <class T>
-T* autoload()
-{
+T* autoload() {
 	return godot::Object::cast_to<T>(get(T::___get_class_name()));
 }
 
