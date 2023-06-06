ea84df42203f3e58c864caa5d51959fd54a42b4c
Colugo <68328892+colugomusic@users.noreply.github.com>
2023-06-06 13:57:13 +0100

    Create scene_helper.hpp

---
+++ include/gdnutil/scene_helper.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <set>
+#include <vector>
+#include <Node.hpp>
+#include "control_helpers.hpp"
+
+namespace gdn {
+
+struct SceneHelper {
+	template <class NodeType = godot::Node>
+	auto get_unknown_children(const godot::Node* root) const {
+		std::vector<NodeType*> out;
+		for (int i = 0; i < root->get_child_count(); i++) {
+			const auto child = godot::Object::cast_to<NodeType>(root->get_child(i));
+			if (child && known_nodes.find(child) == known_nodes.end()) {
+				out.push_back(child);
+			}
+		}
+		return out;
+	}
+	auto calculate_content_width(const godot::Node* root) const -> float {
+		float widest { 0.0f };
+		for (int i = 0; i < root->get_child_count(); i++) {
+			const auto child { godot::Object::cast_to<godot::Control>(root->get_child(i)) };
+			if (!child || !child->is_visible()) {
+				continue;
+			}
+			const auto width { gdn::width(child) };
+			if (width > widest) {
+				widest = width;
+			}
+		}
+		return widest;
+	}
+	std::set<godot::Node*> known_nodes;
+	bool ready { false };
+};
+
+} // gdn
