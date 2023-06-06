0d11740ad5afb9fbb50c5e5f425fdbb6f971c464
Colugo <68328892+colugomusic@users.noreply.github.com>
2023-06-06 13:57:16 +0100

    Update scene.hpp

--- include/gdnutil/scene.hpp
+++ include/gdnutil/scene.hpp
@@ -1,19 +1,13 @@
 #pragma once
 
-#include <set>
-#include <vector>
-#include <Node.hpp>
-#include "control_helpers.hpp"
-#include "macros.hpp"
 #include "packed_scene.hpp"
 
 namespace gdn {
 
-template <typename T, typename ControlType> struct Script;
-template <typename T, typename ScriptType, typename ControlType>
+template <typename T, typename NodeType> struct Script;
+template <typename T, typename ScriptType, typename NodeType>
 struct Scene {
-	using control_type = ControlType;
-	ControlType* root{nullptr};
+	NodeType* root{nullptr};
 	ScriptType* script{nullptr};
 	Scene() = default;
 	Scene(const Scene&) = delete;
@@ -23,7 +17,7 @@ struct Scene {
 		, script{rhs.script}
 		, owned_{rhs.owned_}
 	{
-		reinterpret_cast<Script<T, ControlType>*>(script)->view = static_cast<T*>(this);
+		reinterpret_cast<Script<T, NodeType>*>(script)->view = static_cast<T*>(this);
 		rhs.root = nullptr;
 		rhs.owned_ = false;
 	}
@@ -31,19 +25,19 @@ struct Scene {
 		root = rhs.root;
 		script = rhs.script;
 		owned_ = rhs.owned_;
-		reinterpret_cast<Script<T, ControlType>*>(script)->view = static_cast<T*>(this);
+		reinterpret_cast<Script<T, NodeType>*>(script)->view = static_cast<T*>(this);
 		rhs.root = nullptr;
 		rhs.owned_ = false;
 		return *this;
 	}
-	Scene(PackedScene<ControlType> packed_scene)
+	Scene(PackedScene<NodeType> packed_scene)
 		: root{packed_scene.instance()}
 		, script{godot::Object::cast_to<ScriptType>(root)}
 		, owned_{true}
 	{
 		script->view = static_cast<T*>(this);
 	}
-	Scene(ControlType* node)
+	Scene(NodeType* node)
 		: root{node}
 		, script{godot::Object::cast_to<ScriptType>(root)}
 	{
@@ -62,46 +56,16 @@ struct Scene {
 	}
 private:
 	bool owned_{false};
-	friend struct Script<T, ControlType>;
+	friend struct Script<T, NodeType>;
 };
 
-template <typename View, typename ControlType>
-struct Script : public ControlType {
+template <typename View, typename NodeType>
+struct Script : public NodeType {
 	auto _init() {}
 	~Script() {
 		view->owned_ = false;
 	}
 	View* view;
-};
-
-struct SceneHelper {
-	template <class NodeType = godot::Node>
-	auto get_unknown_children(const godot::Node* root) const {
-		std::vector<NodeType*> out;
-		for (int i = 0; i < root->get_child_count(); i++) {
-			const auto child = godot::Object::cast_to<NodeType>(root->get_child(i));
-			if (child && known_nodes.find(child) == known_nodes.end()) {
-				out.push_back(child);
-			}
-		}
-		return out;
-	}
-	auto calculate_content_width(const godot::Node* root) const -> float {
-		float widest { 0.0f };
-		for (int i = 0; i < root->get_child_count(); i++) {
-			const auto child { godot::Object::cast_to<godot::Control>(root->get_child(i)) };
-			if (!child || !child->is_visible()) {
-				continue;
-			}
-			const auto width { gdn::width(child) };
-			if (width > widest) {
-				widest = width;
-			}
-		}
-		return widest;
-	}
-	std::set<godot::Node*> known_nodes;
-	bool ready { false };
 };
 
 } // gdn
