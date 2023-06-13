6f56396af58d65d7c072236c216d7b62b3477c35
Colugo <68328892+colugomusic@users.noreply.github.com>
2023-06-13 16:50:18 +0100

    Update dirt.hpp

--- include/gdnutil/dirt.hpp
+++ include/gdnutil/dirt.hpp
@@ -4,38 +4,28 @@
 
 namespace gdn {
 
-class Dirt
-{
+class Dirt {
 public:
-
 	Dirt() = default;
-
-	explicit Dirt(godot::CanvasItem* parent)
-		: parent_{parent}
-	{
+	explicit Dirt(godot::CanvasItem* parent) : parent_{parent} {}
+	Dirt(Dirt&& rhs) noexcept : dirty_{rhs.dirty_}, parent_{rhs.parent_} {}
+	auto operator=(Dirt&& rhs) noexcept -> Dirt& {
+		dirty_ = rhs.dirty_;
+		parent_ = rhs.parent_;
+		return *this;
 	}
-
-	auto set_dirty() -> void
-	{
+	auto set_dirty() -> void {
 		dirty_ = true;
-
-		if (parent_)
-		{
+		if (parent_) {
 			parent_->update();
 		}
 	}
-
-	auto check_and_reset() -> bool
-	{
+	auto check_and_reset() -> bool {
 		const auto out{dirty_};
-
 		dirty_ = false;
-
 		return out;
 	}
-
 private:
-
 	bool dirty_{true};
 	godot::CanvasItem* parent_{};
 };
