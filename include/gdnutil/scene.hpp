0ac3cfb1258e14fd8eff3556599e719f82b748d0
Colugo <68328892+colugomusic@users.noreply.github.com>
2023-06-28 21:13:43 +0100

    Update scene.hpp

--- include/gdnutil/scene.hpp
+++ include/gdnutil/scene.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <memory>
 #include <type_traits>
 #include <Node.hpp>
@@ -7,6 +8,7 @@
 
 namespace gdn {
 
+/*
 template <typename T> struct Script;
 template <typename T, typename ScriptType, typename NodeType = godot::Node>
 struct Scene {
@@ -107,6 +109,139 @@ struct Script {
 		}
 	}
 	View* view;
+};
+*/
+
+template <typename T> struct Scene;
+template <typename T> struct Script;
+template <typename T, typename ScriptType, typename NodeType = godot::Node>
+struct Controller {
+	using node_type = NodeType;
+	using script_type = ScriptType;
+	Controller(const Controller&) = delete;
+	Controller(Controller&& rhs) = delete;
+	auto operator=(const Controller&) -> Controller& = delete;
+	auto operator=(Controller&& rhs) -> Controller& = delete;
+	template <typename U, typename e = std::enable_if_t<std::is_base_of_v<NodeType, U>>>
+	Controller(PackedScene<U> packed_scene)
+		: node_{packed_scene.instance()}
+		, root{godot::Object::cast_to<ScriptType>(node_)}
+		, script_{reinterpret_cast<Script<T>*>(root)}
+	{
+		script_->controller = static_cast<T*>(this);
+	}
+	template <typename U,
+		typename e0 = std::enable_if_t<std::is_base_of_v<NodeType, U>>,
+		typename e1 = std::enable_if_t<!std::is_same_v<ScriptType, U>>
+	>
+	Controller(U* node)
+		: root{godot::Object::cast_to<ScriptType>(node)}
+		, script_{reinterpret_cast<Script<T>*>(root)}
+	{
+		script_->controller = static_cast<T*>(this);
+	}
+	Controller(ScriptType* script)
+		: node_{script}
+		, root{script}
+		, script_{reinterpret_cast<Script<T>*>(root)}
+	{
+		script_->controller = static_cast<T*>(this);
+	}
+	~Controller() {
+		if (node_) {
+			node_->free();
+			return;
+		}
+		assert (script_);
+		script_->controller = nullptr;
+	}
+	operator bool() const { return root; }
+private:
+	// If null, this is a non-owning controller (the node
+	// won't be freed when the controller is destroyed.)
+	NodeType* node_{nullptr};
+protected:
+	ScriptType* root{nullptr};
+private:
+	Script<T>* script_{nullptr};
+	friend struct Scene<T>;
+	friend struct Script<T>;
+};
+
+template <typename ControllerType>
+struct Scene {
+public:
+	using node_type = typename ControllerType::node_type;
+	auto close() -> void {
+		controller_.reset();
+		node_ = nullptr;
+	}
+	auto& controller() { return *controller_; }
+	auto& controller() const { return *controller_; }
+	auto& node() { return *node_; }
+	auto& node() const { return *node_; }
+	auto operator->() -> ControllerType* { return controller_.get(); }
+	auto operator->() const -> const ControllerType* { return controller_.get(); }
+	auto operator*() -> ControllerType& { return *controller_; }
+	auto operator*() const -> const ControllerType& { return *controller_; }
+	operator bool() const { return bool(node_); }
+	static auto acquire(node_type* node) -> Scene<ControllerType> {
+		Scene<ControllerType> out;
+		out.acquire_(node);
+		return out;
+	}
+	template <typename... Args>
+	static auto make(Args&&... args) -> Scene<ControllerType> {
+		Scene<ControllerType> out;
+		out.make_(std::forward<Args>(args)...);
+		return out;
+	}
+	template <typename... Args>
+	static auto open(node_type* node, Args&&... args) -> Scene<ControllerType> {
+		Scene<ControllerType> out;
+		out.open_(node, std::forward<Args>(args)...);
+		return out;
+	}
+private:
+	auto acquire_(node_type* node) -> void {
+		auto script{reinterpret_cast<Script<ControllerType>*>(node)};
+		controller_ = script->controller_wkptr.lock();
+		if (controller_) {
+			node_ = node;
+		}
+	}
+	template <typename... Args>
+	auto make_(Args&&... args) -> void {
+		controller_ = std::make_shared<ControllerType>(std::forward<Args>(args)...);
+		controller_->root->controller_wkptr = controller_;
+		node_ = controller_->node_;
+	}
+	template <typename... Args>
+	auto open_(node_type* node, Args&&... args) -> void {
+		auto script{reinterpret_cast<Script<ControllerType>*>(node)};
+		if (script->controller_wkptr.expired()) {
+			controller_ = std::make_shared<ControllerType>(node, std::forward<Args>(args)...);
+			script->controller_wkptr = controller_;
+			node_ = node;
+		}
+		else {
+			acquire_(node);
+		}
+	}
+	std::shared_ptr<ControllerType> controller_;
+	node_type* node_{nullptr};
+};
+
+template <typename ControllerType>
+struct Script {
+	~Script() {
+		if (controller) {
+			controller->node_ = nullptr;
+			controller->root = nullptr;
+		}
+	}
+	ControllerType* controller{nullptr};
+	std::weak_ptr<ControllerType> controller_wkptr;
 };
 
 template <typename NodeType, typename Body>
