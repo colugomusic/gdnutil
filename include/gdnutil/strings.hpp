865d9afb9ea00335f9001ee5aa6ac42cccf8dd0d
Colugo <colugomusic@gmail.com>
2025-02-21 13:10:39 +0000

    Update strings.hpp

--- include/gdnutil/strings.hpp
+++ include/gdnutil/strings.hpp
@@ -23,6 +23,7 @@ STRING(focus_entered);
 STRING(focus_exited);
 STRING(gui_input);
 STRING(id_pressed);
+STRING(index_pressed);
 STRING(item_activated);
 STRING(item_edited);
 STRING(item_rect_changed);
